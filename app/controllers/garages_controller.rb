a07b6ae6ec20a58243cc773bf68834f8ec4d7384
Eric Bakan <eric@ebakan.com>
2014-04-02 16:21:44 -0700

    Initial version of API implementation

--- app/controllers/garages_controller.rb
+++ app/controllers/garages_controller.rb
@@ -1,4 +1,38 @@
 class GaragesController < ApplicationController
   def index
+    respond_to do |format|
+      format.html { render action: "index" }
+      format.json { render json: current_user.garages }
+    end
+  end
+
+  def toggle
+    @garage.open ^= true
+    @garage.save!
+    render json: @garage
+  end
+
+  def add_permission
+    render json: Permission.create!(user: User.find_by_email(params[:email]) || current_user, garage: @garage)
+  end
+
+  def revoke_permission
+    render json: Permission.destroy(user: User.find_by_id(params[:user_id]) || current_user, garage: @garage)
+  end
+
+  def create
+    render json: Garage.create!(user: current_user, name: params[:name])
+  end
+
+  def show
+    render json: @garage
+  end
+
+  def update
+    render json: @garage.update_attributes(params)
+  end
+
+  def destroy
+    render json: @garage.destroy
   end
 end
