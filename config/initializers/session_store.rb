7fa87c839019dc3f4d88a3e298782109ee5e81d8
Eric Bakan <eric@ebakan.com>
2014-04-02 11:28:58 -0700

    Initialized rails application

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Garaje::Application.config.session_store :cookie_store, key: '_garaje_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Garaje::Application.config.session_store :active_record_store
